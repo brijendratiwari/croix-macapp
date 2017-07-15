c9e301a98db70ceda1223df767fec1d6688f4ff0
Ignis IT <euphoriatek2010@@gmail.com>
2017-07-15 17:30:48 +0530

    Initial Commit

---
+++ CorixMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CorixMac
+//
+//  Created by Ignis IT  on 15/07/17.
+//  Copyright © 2017 Ignis IT . All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
